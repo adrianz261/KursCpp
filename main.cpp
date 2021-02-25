a11d0f9599f8ff0cc6004fb5674623e6425130bc
Zuku271 <adrian.zuchowski@outlook.com>
2021-02-25 20:37:50 +0100

    Palindrom

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#include "palindrom.h"
 
 
 int main()
 {
-  std::cout << "Hello World!\n";
+    const std::string text = "kaja";
+    
+    if (isPalindrome(text))
+    {
+        std::cout << text << " jest palindromem." << '\n';
+    }
+    else
+    {
+        std::cout << text << " nie jest palindromem." << '\n';
+    }  
 }
---
+++ palindrom.h
@@ -0,0 +1,21 @@
+#include <string>
+
+bool isPalindrome(const std::string & text)
+{
+  if (text.size() < 3)
+  {
+    return false;
+  }
+
+  const size_t size_of_string = text.size() - 1;
+  const size_t n = text.size() / 2;
+  for (size_t i = 0; i < n; ++i)
+  {
+    if (text.at(i) != text.at(size_of_string - i))
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
